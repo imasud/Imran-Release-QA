c59e20cfb3ce286082b97833a27c9dda74c2319d
imasud <imran.masud@kaplan.com>
2014-03-28 13:57:20 -0400

    Java code

---
+++ FirstJavaClass.java
@@ -0,0 +1,57 @@
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// This is a sample program
+		
+		System.out.print ("First Line");
+		System.out.print("second line");
+		System.out.println("Third Line");
+
+		// datatypes 
+		
+		int i = 100; // integer = 32 bit
+		System.out.println(i);
+		i=1000;
+		System.out.println(i);
+		
+		long z=1992; //integer //64
+		double d=91.234; //integer and decimal both
+		char c = 'e'; 
+		boolean b = true; //true and false values
+		
+		System.out.println(99);
+		System.out.println(2>111);
+		// > <  >+ <+ ++ !=
+		
+		int x=600;
+		int y=800;
+				if(x>y) {
+					System.out.println("x is the greatest "  + x);
+				} else {
+						System.out.println("y is the greatest " + y );
+						
+				}
+		
+				int a1 = 100;
+				int b1 = 2700;
+				int c1 = 3000;
+				
+				if (a1>b1 & a1>c1) {
+					System.out.println(" a1 is the greates " + a1);
+				} else if (b1>c1) {
+					System.out.println("b1 is the greates " + b1);
+				} else {
+					System.out.println("c1 is the greates " + c1);
+				}
+		
+				
+				// string
+		        //  string str = "hello how are u"; 
+		
+		
+		
+		
+	}
+
+}
